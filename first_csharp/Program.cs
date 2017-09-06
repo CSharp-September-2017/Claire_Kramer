f8903bd38a0f04a2a8d5f29998ffbc5ab09cc10a
Claire <kramerclaire@me.com>
2017-09-06 14:35:20 -0700

    Fundamentals I

--- first_csharp/Program.cs
+++ first_csharp/Program.cs
@@ -6,7 +6,24 @@ namespace first_csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = 10;
+            string word = "Hello";
+            bool flag = true;
+            int[] numArray = new int[5];
+            fizzbuzz(10);
+            Random rand = new Random();
+            for(int val = 0; val < 10; val++)
+            {
+                Console.WriteLine(rand.Next(2,8));
+            }
+        }
+
+        public static void fizzbuzz(int limit)
+        {
+            for (int i = 1; i < limit; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
---
+++ fundamentals/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             for (int i = 1; i <= 256; i++)
+            {
+                Console.WriteLine(i);
+            }
+            int x = 1;
+            while (x < 101)
+            {
+                if (x % 5 == 0 && x % 3 == 0)
+                {
+                    x = x + 1;
+                }
+                else if (x % 5 == 0) {
+                    Console.WriteLine(x);
+                    x = x + 1;
+                }
+                else if (x % 3 == 0) {
+                    Console.WriteLine(x);
+                    x = x + 1;
+                }
+                else
+                {
+                    x = x + 1;
+                }
+            }
+            int y = 1;
+            while (y < 101)
+            {
+                if (y % 5 == 0 && y % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                    y = y + 1;
+                }
+                else if (y % 5 == 0) {
+                    Console.WriteLine("Buzz");
+                    y = y + 1;
+                }
+                else if (y % 3 == 0) {
+                    Console.WriteLine("Fizz");
+                    y = y + 1;
+                }
+                else
+                {
+                    y = y + 1;
+                }
+            }
+            Random rand = new Random();
+            for (int z = 0; z < 10; z++)
+            {
+                int num = rand.Next();
+                if (num % 5 == 0 && num % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (num % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (num % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(" ");
+                }
+            }
+        }
+    }
+}
