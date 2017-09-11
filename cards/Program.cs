c438ead5850ddd74aa8c3d7bb10c87582add2918
Claire <kramerclaire@me.com>
2017-09-11 15:31:10 -0400

    deck of cards

---
+++ cards/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cards
+{
+    public class Card 
+    {
+        public string stringVal {
+            get {
+                if(val > 1 && val < 11) 
+                {
+                    return val.ToString();
+                }
+                else if (val == 11)
+                {
+                    return "Jack";
+                }
+                else if (val == 12)
+                {
+                    return "Queen";
+                }
+                else if (val == 13)
+                {
+                    return "King";
+                }
+                else if (val == 1)
+                {
+                    return "Ace";
+                }
+            }
+        }
+        public int val;
+        public string suit;
+        public Card(string cardSuit, int numVal){
+            suit = cardSuit;
+            val = numVal;
+        }
+        public override string ToString(){
+            return stringVal + " of " + suit;
+        }
+    }
+    public class Deck
+    {
+        public List<object> cards;
+        public Deck()
+        {
+            car
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
