927cefe15c0bb4f3e65c33aca53308568c355e91
Claire <kramerclaire@me.com>
2017-09-11 19:10:43 -0400

    Better Version of Deck of Cards

--- cards/Program.cs
+++ cards/Program.cs
@@ -7,7 +7,7 @@ namespace cards
     {
         public string stringVal {
             get {
-                if(val > 1 && val < 11) 
+                if(val > 1 && val < 11)
                 {
                     return val.ToString();
                 }
@@ -27,6 +27,10 @@ namespace cards
                 {
                     return "Ace";
                 }
+                else
+                {
+                    return "Joker";
+                }
             }
         }
         public int val;
@@ -35,23 +39,83 @@ namespace cards
             suit = cardSuit;
             val = numVal;
         }
-        public override string ToString(){
+
+        public override string ToString() {
             return stringVal + " of " + suit;
         }
     }
     public class Deck
     {
-        public List<object> cards;
-        public Deck()
-        {
-            car
+        private List<Card> cards;
+        
+        public Deck() {
+            Reset();
+        }
+
+        public Card Deal() {
+            if(cards.Count > 0) {
+                Card temp = cards[0];
+                cards.RemoveAt(0);
+                return temp;
+            }
+            return null;
+        }
+
+        public Deck Shuffle() {
+            Random rand = new Random();
+            for (int idx = cards.Count -1; idx > 0; idx--) {
+                int randIdx = rand.Next(idx);
+                Card temp = cards[randIdx];
+                cards[randIdx] = cards[idx];
+                cards[idx] = temp;
+            }
+            return this;
+        }
+
+        public Deck Reset() {
+            cards = new List<Card>();
+            string[] suits = new string[4] {"Hearts", "Diamonds", "Clubs", "Spades"};
+            foreach (string suit in suits) {
+                for(int val = 1; val <= 13; val++) {
+                    cards.Add(new Card(suit, val));
+                }
+            }
+            return this;
+        }
+
+        public override string ToString() {
+            string info = "";
+            foreach (Card card in cards) {
+                info += card + "\n";
+            }
+            return info;
+        }
+    }
+
+    public class Player {
+        public string name;
+        private List<Card> hand;
+        
+        public Player(string newName) {
+            hand = new List<Card>();
+            name = newName;
+        }
+
+        public void Draw(Deck currentDeck) {
+            hand.Add(currentDeck.Deal());
+        }
+
+        public Card Discard(int handIdx) {
+            Card temp = hand[handIdx];
+            hand.RemoveAt(handIdx);
+            return temp;
         }
     }
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Deck myDeck = new Deck();
         }
     }
 }
