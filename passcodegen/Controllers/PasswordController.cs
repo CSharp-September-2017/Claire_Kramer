473d37cbce422fbc642e3bc78a99fab588679120
Claire <kramerclaire@me.com>
2017-09-19 10:27:26 -0700

    Random Password Generator

---
+++ passcodegen/Controllers/PasswordController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System;
+
+namespace passcodegen.Controllers {
+    public class PasswordController : Controller {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index() {
+            int? Count = HttpContext.Session.GetInt32("Count");
+            if(Count == null) {
+                Count = 0;
+            }
+            Count += 1;
+            Random random = new Random();
+            string characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            string result = "";
+            for (int i = 0; i < 14; i++) {
+                result = result + characters[random.Next(characters.Length)];
+            }
+            ViewBag.Password = result;
+            ViewBag.Count = Count;
+            HttpContext.Session.SetInt32("Count", (int)Count);
+            return View();
+        }
+    }
+}
