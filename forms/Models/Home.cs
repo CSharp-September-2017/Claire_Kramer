2a7bdf84837e7991087973a2673487030c539ae3
Claire <kramerclaire@me.com>
2017-10-05 11:40:34 -0400

    Form Val

---
+++ forms/Models/Home.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace forms.Models {
+    public class Home {
+        [Required]
+        [MinLength(4)]
+        public string FirstName { get; set; }
+        [Required]
+        [MinLength(4)]
+        public string LastName { get; set; }
+        [Required]
+        [Range(1,99)]
+        public int Age { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
